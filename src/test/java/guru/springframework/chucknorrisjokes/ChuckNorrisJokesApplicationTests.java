f1c8b54c264c4509882ac31a0427a2ded78116c6
PierreSQS <nomo_messina@web.de>
2021-04-07 12:11:37 +0200

    initial add from Spring Initializr. Closes springframeworkguru#1

---
+++ src/test/java/guru/springframework/chucknorrisjokes/ChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.chucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChuckNorrisJokesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
